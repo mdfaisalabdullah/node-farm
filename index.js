526ff17df53f6cda90d5ff99905f5c6358329c76
Faisal <md.faisal1899@yahoo.com>
2022-01-25 18:04:08 +0600

    read and wrote file asynchronusly in node

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const fs = require("fs");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
-const textOut = `This is we know about avocado: ${textIn} \n create: ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is we know about avocado: ${textIn} \n create: ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
 
-console.log("File Written");
+// console.log("File Written");
+
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  if (err) return;
+  fs.readFile(`./txt/${data}.txt`, "utf-8", (err, data2) => {
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      fs.writeFile("./txt/final.txt", `${data2}....${data3}`, "utf-8", err => {
+        if (err) console.log("ERROR");
+      });
+    });
+  });
+});
+
+console.log("loading..........");
